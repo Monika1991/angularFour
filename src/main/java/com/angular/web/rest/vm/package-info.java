c2e9782bbbacd4a27536e64c0410cfbc4ce0274b
monika sharma <monika.hartron@gmail.com>
2017-08-18 15:16:20 +0530

    angular4

---
+++ src/main/java/com/angular/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.angular.web.rest.vm;
---
+++ src/test/java/com/angular/service/UserServiceIntTest.java
@@ -0,0 +1,43 @@
+package com.angular.service;
+
+import com.angular.AbstractCassandraTest;
+import com.angular.JhipsterApp;
+import com.angular.domain.User;
+import com.angular.config.Constants;
+import com.angular.repository.UserRepository;
+import com.angular.service.dto.UserDTO;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+import java.time.Instant;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * Test class for the UserResource REST controller.
+ *
+ * @see UserService
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = JhipsterApp.class)
+public class UserServiceIntTest extends AbstractCassandraTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void assertThatAnonymousUserIsNotGet() {
+        final List<UserDTO> allManagedUsers = userService.getAllManagedUsers();
+        assertThat(allManagedUsers.stream()
+            .noneMatch(user -> Constants.ANONYMOUS_USER.equals(user.getLogin())))
+            .isTrue();
+    }
+}
